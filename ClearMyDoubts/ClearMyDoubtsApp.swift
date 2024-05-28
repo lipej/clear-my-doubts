361ddab79b702ad0686a421991ad7a637edf6fec
Felipe José <dasilvalipejose@gmail.com>
2024-05-28 12:01:51 -0300

    Initial Commit

---
+++ ClearMyDoubts/ClearMyDoubtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearMyDoubtsApp.swift
+//  ClearMyDoubts
+//
+//  Created by Felipe José on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ClearMyDoubtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClearMyDoubts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClearMyDoubts
+//
+//  Created by Felipe José on 28/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
